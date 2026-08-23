139f7b793821d8afebe8436a60a21fbfb1d466a1
hossamasr <hossamasrw@gmail.com>
2026-08-23 11:35:14 +0300

    completing tokenization part

--- c/common.h
+++ c/common.h
@@ -6,4 +6,5 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
+#define DEBUG_TRACE_EXECUTION
 #endif
---
+++ c/compiler.h
@@ -0,0 +1,6 @@
+#ifndef casrlang_compiler_h
+#define casrlang_compiler_h
+
+void compile(const char* source);
+
+#endif
